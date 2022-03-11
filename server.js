0ab7a45aabb6063e9de0fb118cb6fd1c48a4ddda
Your Name <you@example.com>
2022-03-11 13:44:38 +0200

    add DB connection

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB Connection successful!'));
 
 // Start Server
-app.listen(3000, () => {
-    console.log("App running on port 3000   ...")
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port} ...`)
 });
